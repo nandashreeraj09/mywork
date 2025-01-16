a9362282a37dd951b2d11fdd82a34094692afc21
Nandlal Rajak <167088060+nandashreeraj09@users.noreply.github.com>
2025-01-16 13:54:38 +0530

    Update constant.php

--- constant.php
+++ constant.php
@@ -1,22 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'tg2trackerksdhfh_user');
-define('DB_PASSWORD', '@+~oak*IFkzc');
-define('DB_DATABASE', 'tg2trackerksdhfh_tracker_guru'); 
-define('AFFLICATEID', 'NjVlZWRhN2FjODgxNw==');
-define('PARTNERID', 'P3048');
-define('MYWEBSITE_URL','https://tg2.trackerguru.in/');
-
-
-
-/************** Holiday DB Constants ***************/
-//define('DB_SERVER_LOCAL_TOUR', 'localhost');
-//define('DB_USERNAME_LOCAL_TOUR', 'excuriaksdhfdsfh_sonic');
-//define('DB_PASSWORD_LOCAL_TOUR', 'KpN{]OIwe^?6');
-//define('DB_DATABASE_LOCAL_TOUR', 'excuriaksdhfdsfh_package_tour');
-
-
-
-
+define('DB_USERNAME', 'db_user');
+define('DB_PASSWORD', 'db_password');
+define('DB_DATABASE', 'db_databasename'); 
 
 ?>
